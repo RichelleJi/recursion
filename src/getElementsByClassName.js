e815881656e712b192155497e586f228a57927ac
RRTT <RT@Admins-MacBook-Pro.local>
2018-05-30 20:20:48 -0400

    recursion submission

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,26 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+
+var getElementsByClassName = function(className){
   // your code here
+  var result = [];
+  function verify (node) {
+    if (node) {
+  		if (node.length) {
+  			for (var i = 0; i < node.length; i++) {
+  				verify(node[i]);
+  			}
+  		} else {
+  			if (node.classList) {
+  				if (node.classList.contains(className)) {
+  					result.push(node);
+  				}
+  				verify(node.childNodes);
+  			}
+ 	 	}
+ 	 }
+ 	}
+  verify(document.body);
+  return result;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,39 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'string')
+    return '"' + obj + '"';
+
+  if(typeof(obj) === undefined || typeof(obj)== "symbol" || typeof(obj) == "function" )
+      return undefined; 
+  
+  if(typeof(obj) === 'number' || obj === null || typeof(obj) === 'boolean')
+      return '' + obj;
+
+  if (Array.isArray(obj)) {
+    for(var i = 0; i < obj.length; i++) {
+      obj[i] = stringifyJSON(obj[i]);
+    }
+    return '[' + obj + ']';
+  }
+
+  if (typeof obj === 'object') {
+    var keys = Object.keys(obj);
+    var str = '';
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      if (key === 'functions' || key === undefined) {
+        return '{}';
+      } else {
+        if (i === keys.length - 1) {
+          str += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+        } else {
+          str += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+        }
+      }
+    }
+    return '{' + str + '}';
+  }
 };
